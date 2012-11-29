b59673efe14f357552dbc7a3200e594288798f7b
Paul Lam <paul@quantisan.com>
2012-11-29 11:35:05 +0100

    start Matrix class implements ISeq

---
+++ src/java/clatrix/Matrix.java
@@ -0,0 +1,49 @@
+package clatrix;
+
+import clojure.lang.*;
+
+/*
+* Based on https://github.com/forward/incanter-BLAS by Antonio Garrote
+*
+* */
+ public class Matrix implements ISeq {
+    @Override
+    public Object first() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ISeq next() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ISeq more() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int count() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ISeq cons(Object o) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public IPersistentCollection empty() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean equiv(Object o) {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ISeq seq() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
