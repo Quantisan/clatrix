e88ac9b444857d2c6eb860f5ec8d08e020cab760
Paul Lam <paul@quantisan.com>
2012-11-29 12:03:52 +0100

    Matrix implements ISeq

--- src/java/clatrix/Matrix.java
+++ src/java/clatrix/Matrix.java
@@ -1,15 +1,67 @@
 package clatrix;
 
+import org.jblas.*;
 import clojure.lang.*;
 
 /*
 * Based on https://github.com/forward/incanter-BLAS by Antonio Garrote
 *
 * */
- public class Matrix implements ISeq {
+public class Matrix implements ISeq {
+    public boolean oneDimensional = false;
+    IPersistentMap meta;
+    private DoubleMatrix matrix;
+
+    /*********************
+     * Matrix constructors
+     *********************/
+    public Matrix(int nrow, int ncol) {
+        this(nrow,ncol,0);
+    }
+
+    public Matrix(int nrow, int ncol, Number initValue) {
+        this.matrix = new DoubleMatrix(nrow,ncol);
+        this.meta = null;
+        for(int i = 0; i < nrow; i++)
+            for(int j = 0; j < ncol; j++)
+                matrix.put(i, j, initValue.doubleValue());
+        if(matrix.rows == 1 || matrix.columns == 1)
+            this.oneDimensional = true;
+    }
+
+    public Matrix(DoubleMatrix mat) {
+        this(mat.rows, mat.columns);
+        this.assign(mat);
+    }
+
+
+    /**************************************
+     * MATRIX METHODS
+     **************************************/
+
+    public void assign(DoubleMatrix matrix) {
+        this.matrix = matrix.dup();
+        if(matrix.columns == 1 || matrix.rows == 1) {
+            this.oneDimensional = true;
+        }
+    }
+
+    /**************************************
+     * ISEQ METHODS
+     **************************************/
     @Override
     public Object first() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        if(this.matrix.rows == 0 || this.matrix.columns == 0) return(null);
+
+        if(this.oneDimensional && (matrix.columns == 1 || matrix.rows == 1))
+            return(matrix.get(0, 0));
+        else {
+            int[] subset = new int[matrix.columns];
+            for(int i=0; i<subset.length; i++)
+                subset[i] = i;
+
+            return new Matrix(matrix.get(0, subset));
+        }
     }
 
     @Override
